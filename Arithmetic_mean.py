7d15f930edd1ce43cb0a3307e7b36cd78cc2e19d
Cédric Gaudissart <58433068+cedric-gaudissart@users.noreply.github.com>
2020-04-12 20:12:10 +0200

    Add files via upload
    Data science coaching script to compute the arithmetic mean.

---
+++ Arithmetic_mean.py
@@ -0,0 +1,28 @@
+#%% IMPORT PACKAGES
+
+import numpy as np
+import pandas as pd
+
+
+
+#%% CREATE OBJECTS
+
+x = [1, 2, 3]   
+
+n = len(x)                  # x is a column of length 3
+
+
+
+#%% COMPUTE
+
+# Option 1
+
+1/n * sum(x)
+
+
+# Option 2 
+
+np.mean(x)                 
+
+
+
